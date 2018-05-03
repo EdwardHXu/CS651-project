602b1fbc9c47f49e25acfdda7a370964863284cc
EdwardXu <edwardxu@bu.edu>
2018-05-02 23:22:09 -0400

    gsubmit version

--- src/main/resources/static/js/restClient.js
+++ src/main/resources/static/js/restClient.js
@@ -1,14 +1,7 @@
 "use strict";
 
-var testMode = false;
-
 function getAllAgents() {
     sendHttpRequest("GET", "agent/all", null, displayAllAgents);
-
-    if (testMode) {
-        displayAllAgents('[{"name":"Agent1","port":1001,"blockchain":[{"index":0,"creator":"Agent1","timestamp":1502193341671,"hash":"4f99b67b06b6831886815ffe66a55be2e34dcefdfc16b6214710313062a8a480","previousHash":"ROOT_HASH"}]}' +
-            ', {"name":"Agent2","port":1002,"blockchain":[{"index":1,"creator":"Agent2","timestamp":1502193341671,"hash":"4f99b67b06b6831886815ffe66a55be2e34dcefdfc16b6214710313062a8a480","previousHash":"ROOT_HASH"}]}]');
-    }
 }
 
 function deleteAllAgents() {
@@ -22,10 +15,6 @@ function createAgent() {
     // var name = "Agent" + idx;
     var port = 3000 + idx;
     sendHttpRequest("POST", "agent?name=" + name + "&port=" + port, null, displayAgent);
-
-    if (testMode) {
-        displayAgent('{"name":"Agent1","port":1001,"blockchain":[{"index":2,"creator":"Agent1","timestamp":1502193341671,"hash":"4f99b67b06b6831886815ffe66a55be2e34dcefdfc16b6214710313062a8a480","previousHash":"ROOT_HASH"}]}');
-    }
 }
 
 function deleteAgent(name) {
@@ -42,9 +31,6 @@ function mine(name) {
     var message = prompt("Please input your chatting messages.", platformMessage);
     sendHttpRequest("POST", "agent/mine?agent=" + name + "&message=" + message, null, getAllAgents);
 
-    if (testMode) {
-        displayBlock('{"index":1,"creator":"Agent1","timestamp":1502194172250,"hash":"2461f27f811df15a969391c70f136869a282224e8cc6fe8b628d16a499515d21","previousHash":"4f99b67b06b6831886815ffe66a55be2e34dcefdfc16b6214710313062a8a480"}');
-    }
 }
 
 function sendHttpRequest(action, url, data, callback) {
